6169825a2d0090c73ae59b2ae6c17d8ac2ed755a
Rafael Tovar <rafael.tovar@galgus.net>
2024-02-25 17:28:52 +0100

    fix: hover error small size

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -7,7 +7,7 @@ const Button = (props: Props) => {
     <button
       disabled={!!selectedLevel}
       onClick={() => onClick(level)}
-      className={`px-3.5 py-1 text-4xl font-semibold rounded hover:opacity-100 cursor-pointer	max-sm:text-3xl max-sm:max-w-[72px] max-sm:place-self-center ${classes} ${
+      className={`px-3.5 py-1 text-4xl font-semibold rounded sm:hover:opacity-100 cursor-pointer	max-sm:text-3xl max-sm:max-w-[72px] max-sm:place-self-center ${classes} ${
         selectedLevel === level ? "opacity-100" : "opacity-55"
       }`}
     >
