52f57335a17e203522860e316daf8a5376074518
Rafael <rafael.tovar@galgus.net>
2024-03-24 17:32:33 +0000

    style: fix animation

--- src/app/pages/Main/components/WordInfo/index.tsx
+++ src/app/pages/Main/components/WordInfo/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Tooltip } from 'react-tooltip';
 import './style.css';
 import { Props } from './types';
@@ -5,10 +6,20 @@ import { Props } from './types';
 const WordInfo = (props: Props) => {
   const { word, type, desc } = props;
 
+  useEffect(() => {
+    setTimeout(() => {
+      const item = document.querySelectorAll('.text')[0];
+      if (item) {
+        item.classList.remove('whitespace-nowrap');
+        item.classList.remove('whitespace-normal');
+      }
+    }, 2000);
+  }, [word]);
+
   return (
     <div className="flex flex-col justify-self-center items-center break-all">
       <h1
-        className="text-7xl font-semibold text-white capitalize max-sm:text-3xl text"
+        className="text-7xl font-semibold text-white capitalize max-sm:text-3xl text whitespace-nowrap break-normal"
         data-tooltip-id="tooltip"
         data-tooltip-content={`${desc[0].toUpperCase()}${desc
           .slice(1)
@@ -17,7 +28,7 @@ const WordInfo = (props: Props) => {
         <span className="border-bottom-dotted">{word}</span>
       </h1>
       <span className="text-4xl text-white max-sm:text-2xl">as</span>
-      <h1 className="text-7xl font-semibold text-white capitalize max-sm:text-3xl text">
+      <h1 className="text-7xl font-semibold text-white capitalize max-sm:text-3xl text whitespace-nowrap">
         {type}
       </h1>
       <Tooltip id="tooltip" className="tooltip"></Tooltip>
