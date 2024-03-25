b2a32862e59ea6bdfab7cff2134745a13f832a32
Rafael <rafael.tovar@galgus.net>
2024-03-25 09:13:11 +0000

    style: reduce font size

--- src/app/pages/Main/components/WordInfo/index.tsx
+++ src/app/pages/Main/components/WordInfo/index.tsx
@@ -19,16 +19,18 @@ const WordInfo = (props: Props) => {
   return (
     <div className="flex flex-col justify-self-center items-center break-all">
       <h1
-        className="text-7xl font-semibold text-white capitalize max-sm:text-3xl text whitespace-nowrap break-normal"
+        className="text-6xl font-semibold text-white max-sm:text-3xl text whitespace-nowrap break-normal"
         data-tooltip-id="tooltip"
         data-tooltip-content={`${desc[0].toUpperCase()}${desc
           .slice(1)
           .toLowerCase()}`}
       >
-        <span className="border-bottom-dotted">{word}</span>
+        <span className="border-bottom-dotted">{`${word[0].toUpperCase()}${word.slice(
+          1
+        )}`}</span>
       </h1>
       <span className="text-4xl text-white max-sm:text-2xl">as</span>
-      <h1 className="text-7xl font-semibold text-white capitalize max-sm:text-3xl text whitespace-nowrap">
+      <h1 className="text-6xl font-semibold text-white capitalize max-sm:text-3xl text whitespace-nowrap">
         {type}
       </h1>
       <Tooltip id="tooltip" className="tooltip"></Tooltip>
